2f366aaf614efa6c02d8fdec0dfb3ff01839f8c7
Arefmt <arefm.ven@gmail.com>
2020-02-20 10:41:30 +0100

    Version 1.1 Mapper + Driver

--- Tweets/src/main/java/Tweets_Mapper.java
+++ Tweets/src/main/java/Tweets_Mapper.java
@@ -1,13 +1,15 @@
 import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-
 import org.apache.hadoop.mapreduce.Mapper;
 import org.json.*;
 
-public class Tweets_Mapper {
 
-    public void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException{
+public class Tweets_Mapper extends Mapper<Object, Text, Text, IntWritable> {
+
+    public void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
 
         String _user_id;
         String _id;
@@ -17,7 +19,7 @@ public class Tweets_Mapper {
 
         String line = value.toString();
         String[] tuple = line.split("\\n");
-        try{
+        try {
             for (String s : tuple) {
                 JSONObject obj = new JSONObject(s);
                 _id = obj.getString("id");
@@ -27,34 +29,8 @@ public class Tweets_Mapper {
                 _location = obj.getString("location");
                 context.write(Integer.parseInt(_user_id), Integer.parseInt(_id) + "\t" + _created_at + "\t" + _location + "\t" + _tweet_text);
             }
-        }catch(JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
-
-    public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        if (args.length != 2) {
-            System.err.println("Usage: CombineBooks <in> <out>");
-            System.exit(2);
-        }
-
-        Job job = new Job(conf, "CombineBooks");
-        job.setJarByClass(CombineBooks.class);
-        job.setMapperClass(Map.class);
-        job.setReducerClass(Reduce.class);
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
-        job.setOutputKeyClass(NullWritable.class);
-        job.setOutputValueClass(Text.class);
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
-
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-
-
-
 }
